dc5f121c6412cc910fd884d1fcab5b7ac4ad8fdf
Asim <asim@chuhnk.me>
2015-11-26 20:43:33 +0000

    Fix the breaking build

--- examples/client/main.go
+++ examples/client/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/micro/go-micro/client"
 	"github.com/micro/go-micro/cmd"
@@ -10,6 +11,42 @@ import (
 	"golang.org/x/net/context"
 )
 
+// wrapper example code
+
+// log wrapper logs every time a request is made
+type logWrapper struct {
+	client.Client
+}
+
+func (l *logWrapper) Call(ctx context.Context, req client.Request, rsp interface{}) error {
+	md, _ := c.GetMetadata(ctx)
+	fmt.Printf("[Log Wrapper] ctx: %v service: %s method: %s\n", md, req.Service(), req.Method())
+	return l.Client.Call(ctx, req, rsp)
+}
+
+// trace wrapper attaches a unique trace ID - timestamp
+type traceWrapper struct {
+	client.Client
+}
+
+func (t *traceWrapper) Call(ctx context.Context, req client.Request, rsp interface{}) error {
+	ctx = c.WithMetadata(ctx, map[string]string{
+		"X-Trace-Id": fmt.Sprintf("%d", time.Now().Unix()),
+	})
+	return t.Client.Call(ctx, req, rsp)
+}
+
+// Implements client.Wrapper as logWrapper
+func logWrap(c client.Client) client.Client {
+	return &logWrapper{c}
+}
+
+// Implements client.Wrapper as traceWrapper
+func traceWrap(c client.Client) client.Client {
+	return &traceWrapper{c}
+}
+
+// publishes a message
 func pub() {
 	msg := client.NewPublication("topic.go.micro.srv.example", &example.Message{
 		Say: "This is a publication",
@@ -82,10 +119,29 @@ func stream() {
 func main() {
 	cmd.Init()
 
+	fmt.Println("\n--- Call example ---\n")
 	for i := 0; i < 10; i++ {
 		call(i)
 	}
 
+	fmt.Println("\n--- Streamer example ---\n")
 	stream()
+
+	fmt.Println("\n--- Publisher example ---\n")
 	pub()
+
+	fmt.Println("\n--- Wrapper example ---\n")
+
+	// Wrap the default client
+	client.DefaultClient = logWrap(client.DefaultClient)
+
+	call(0)
+
+	// Wrap using client.Wrap option
+	client.DefaultClient = client.NewClient(
+		client.Wrap(traceWrap),
+		client.Wrap(logWrap),
+	)
+
+	call(1)
 }
