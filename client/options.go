8ab77fc3d7bed1e25eea06a6fbed989d3eec104b
Asim <asim@chuhnk.me>
2015-11-26 12:51:53 +0000

    Comments for options

--- client/options.go
+++ client/options.go
@@ -14,30 +14,35 @@ type options struct {
 	transport   transport.Transport
 }
 
+// Broker to be used for pub/sub
 func Broker(b broker.Broker) Option {
 	return func(o *options) {
 		o.broker = b
 	}
 }
 
+// Codec to be used to encode/decode requests for a given content type
 func Codec(contentType string, cf CodecFunc) Option {
 	return func(o *options) {
 		o.codecs[contentType] = cf
 	}
 }
 
+// Default content type of the client
 func ContentType(ct string) Option {
 	return func(o *options) {
 		o.contentType = ct
 	}
 }
 
+// Registry to find nodes for a given service
 func Registry(r registry.Registry) Option {
 	return func(o *options) {
 		o.registry = r
 	}
 }
 
+// Transport to use for communication e.g http, rabbitmq, etc
 func Transport(t transport.Transport) Option {
 	return func(o *options) {
 		o.transport = t
