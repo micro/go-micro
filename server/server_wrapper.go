9c17e64ef16405b052bc2f0cc509aa672c8f0667
Asim <asim@chuhnk.me>
2015-12-02 21:16:44 +0000

    add comments for wrappers

--- server/server_wrapper.go
+++ server/server_wrapper.go
@@ -4,10 +4,18 @@ import (
 	"golang.org/x/net/context"
 )
 
+// HandlerFunc represents a single method of a handler. It's used primarily
+// for the wrappers. What's handed to the actual method is the concrete
+// request and response types.
 type HandlerFunc func(ctx context.Context, req Request, rsp interface{}) error
 
+// SubscriberFunc represents a single method of a subscriber. It's used primarily
+// for the wrappers. What's handed to the actual method is the concrete
+// publication message.
 type SubscriberFunc func(ctx context.Context, msg Publication) error
 
+// HandlerWrapper wraps the HandlerFunc and returns the equivalent
 type HandlerWrapper func(HandlerFunc) HandlerFunc
 
+// SubscriberWrapper wraps the SubscriberFunc and returns the equivalent
 type SubscriberWrapper func(SubscriberFunc) SubscriberFunc
