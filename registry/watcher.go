011a3dc9e3e6eee655e0308af24322f2ff5312c8
Asim <asim@chuhnk.me>
2015-12-05 02:05:06 +0000

    Add watcher comments

--- registry/watcher.go
+++ registry/watcher.go
@@ -1,10 +1,15 @@
 package registry
 
+// Watcher is an interface that returns updates
+// about services within the registry.
 type Watcher interface {
+	// Next is a blocking call
 	Next() (*Result, error)
 	Stop()
 }
 
+// Result is returned by a call to Next on
+// the watcher. Actions can be create, update, delete
 type Result struct {
 	Action  string
 	Service *Service
