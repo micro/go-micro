b9f4e17d4cc3a97ce93e57d81ed782cb8038f632
Asim <asim@chuhnk.me>
2016-01-28 18:24:56 +0000

    Meta key change

--- metadata/metadata.go
+++ metadata/metadata.go
@@ -4,26 +4,22 @@ import (
 	"golang.org/x/net/context"
 )
 
-type contextKeyT string
-
-const (
-	contextKey = contextKeyT("github.com/micro/go-micro/metadata")
-)
+type metaKey struct{}
 
 type Metadata map[string]string
 
 func FromContext(ctx context.Context) (Metadata, bool) {
-	md, ok := ctx.Value(contextKey).(Metadata)
+	md, ok := ctx.Value(metaKey{}).(Metadata)
 	return md, ok
 }
 
 func NewContext(ctx context.Context, md Metadata) context.Context {
-	if emd, ok := ctx.Value(contextKey).(Metadata); ok {
+	if emd, ok := ctx.Value(metaKey{}).(Metadata); ok {
 		for k, v := range emd {
 			if _, ok := md[k]; !ok {
 				md[k] = v
 			}
 		}
 	}
-	return context.WithValue(ctx, contextKey, md)
+	return context.WithValue(ctx, metaKey{}, md)
 }
