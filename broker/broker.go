a53979dfed94ba472fdeb3353f8c37b4a5e1ebea
Asim <asim@chuhnk.me>
2016-01-30 21:18:57 +0000

    Add broker comment

--- broker/broker.go
+++ broker/broker.go
@@ -1,5 +1,8 @@
 package broker
 
+// Broker is an interface used for asynchronous messaging.
+// Its an abstraction over various message brokers
+// {NATS, RabbitMQ, Kafka, ...}
 type Broker interface {
 	Options() Options
 	Address() string
