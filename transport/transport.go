9f0dd3dc8ca5bf9f3db38d112b8caa2a9d7324ab
Asim <asim@chuhnk.me>
2016-01-30 21:19:55 +0000

    Add transport comment

--- transport/transport.go
+++ transport/transport.go
@@ -27,6 +27,9 @@ type Listener interface {
 	Accept(func(Socket)) error
 }
 
+// Transport is an interface which is used for communication between
+// services. It uses socket send/recv semantics and had various
+// implementations {HTTP, RabbitMQ, NATS, ...}
 type Transport interface {
 	Dial(addr string, opts ...DialOption) (Client, error)
 	Listen(addr string, opts ...ListenOption) (Listener, error)
