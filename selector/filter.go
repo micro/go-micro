7c3ce60ce6ba039b1525f97e6d8f4a14550ec7da
Asim <asim@chuhnk.me>
2016-04-23 20:57:46 +0100

    Add label and version filters

---
+++ selector/filter.go
@@ -0,0 +1,54 @@
+package selector
+
+import (
+	"github.com/micro/go-micro/registry"
+)
+
+// LabelFilter is a label based Select Filter which will
+// only return services with the label specified.
+func LabelFilter(key, val string) Filter {
+	return func(old []*registry.Service) []*registry.Service {
+		var services []*registry.Service
+
+		for _, service := range old {
+			serv := new(registry.Service)
+			var nodes []*registry.Node
+
+			for _, node := range service.Nodes {
+				if node.Metadata == nil {
+					continue
+				}
+
+				if node.Metadata[key] == val {
+					nodes = append(nodes, node)
+				}
+			}
+
+			// only add service if there's some nodes
+			if len(nodes) > 0 {
+				// copy
+				*serv = *service
+				serv.Nodes = nodes
+				services = append(services, serv)
+			}
+		}
+
+		return services
+	}
+}
+
+// VersionFilter is a version based Select Filter which will
+// only return services with the version specified.
+func VersionFilter(version string) Filter {
+	return func(old []*registry.Service) []*registry.Service {
+		var services []*registry.Service
+
+		for _, service := range old {
+			if service.Version == version {
+				services = append(services, service)
+			}
+		}
+
+		return services
+	}
+}
