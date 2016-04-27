1340e81235fc17b075612b8dcf8f1d21c76c6dda
Asim <asim@chuhnk.me>
2016-04-28 00:07:25 +0100

    Remove logging

--- broker/mock/mock_test.go
+++ broker/mock/mock_test.go
@@ -18,8 +18,6 @@ func TestBroker(t *testing.T) {
 	count := 10
 
 	fn := func(p broker.Publication) error {
-		m := p.Message()
-		t.Logf("Received message id %s %+v for topic %s", m.Header["id"], m, p.Topic())
 		return nil
 	}
 
@@ -37,7 +35,6 @@ func TestBroker(t *testing.T) {
 			Body: []byte(`hello world`),
 		}
 
-		t.Logf("Sending message %d %+v for topic %s", i, message, topic)
 		if err := b.Publish(topic, message); err != nil {
 			t.Fatalf("Unexpected error publishing %d", i)
 		}
