50eb97129e16402287217d4461f517ccced1bbbf
Asim <asim@chuhnk.me>
2016-05-01 21:30:14 +0100

    Add watcher test

--- registry/mdns/watcher_test.go
+++ registry/mdns/watcher_test.go
@@ -2,12 +2,11 @@ package mdns
 
 import (
 	"testing"
-	"time"
 
 	"github.com/micro/go-micro/registry"
 )
 
-func TestMDNS(t *testing.T) {
+func TestWatcher(t *testing.T) {
 	testData := []*registry.Service{
 		&registry.Service{
 			Name:    "test1",
@@ -56,6 +55,12 @@ func TestMDNS(t *testing.T) {
 	// new registry
 	r := NewRegistry()
 
+	w, err := r.Watch()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer w.Stop()
+
 	for _, service := range testData {
 		// register service
 		if err := r.Register(service); err != nil {
@@ -63,10 +68,16 @@ func TestMDNS(t *testing.T) {
 		}
 
 		// get registered service
-		s, err := r.GetService(service.Name)
+		res, err := w.Next()
 		if err != nil {
 			t.Fatal(err)
 		}
+
+		if res.Action != "create" {
+			t.Fatal("Expected create action for %s", service.Name)
+		}
+
+		s := []*registry.Service{res.Service}
 
 		if len(s) != 1 {
 			t.Fatalf("Expected one result for %s got %d", service.Name, len(s))
@@ -100,34 +111,53 @@ func TestMDNS(t *testing.T) {
 		}
 	}
 
-	services, err := r.ListServices()
-	if err != nil {
-		t.Fatal(err)
-	}
-
 	for _, service := range testData {
-		var seen bool
-		for _, s := range services {
-			if s.Name == service.Name {
-				seen = true
-				break
-			}
-		}
-		if !seen {
-			t.Fatalf("Expected service %s got nothing", service.Name)
-		}
-
 		// deregister
 		if err := r.Deregister(service); err != nil {
 			t.Fatal(err)
 		}
 
-		time.Sleep(time.Millisecond * 5)
+		// get registered service
+		res, err := w.Next()
+		if err != nil {
+			t.Fatal(err)
+		}
 
-		// check its gone
-		s, _ := r.GetService(service.Name)
-		if len(s) > 0 {
-			t.Fatalf("Expected nothing got %+v", s[0])
+		if res.Action != "delete" {
+			t.Fatalf("Expected create action for %s", service.Name)
+		}
+
+		s := []*registry.Service{res.Service}
+
+		if len(s) != 1 {
+			t.Fatalf("Expected one result for %s got %d", service.Name, len(s))
+
+		}
+
+		if s[0].Name != service.Name {
+			t.Fatalf("Expected name %s got %s", service.Name, s[0].Name)
+		}
+
+		if s[0].Version != service.Version {
+			t.Fatalf("Expected version %s got %s", service.Version, s[0].Version)
+		}
+
+		if len(s[0].Nodes) != 1 {
+			t.Fatalf("Expected 1 node, got %d", len(s[0].Nodes))
+		}
+
+		node := s[0].Nodes[0]
+
+		if node.Id != service.Nodes[0].Id {
+			t.Fatalf("Expected node id %s got %s", service.Nodes[0].Id, node.Id)
+		}
+
+		if node.Address != service.Nodes[0].Address {
+			t.Fatalf("Expected node address %s got %s", service.Nodes[0].Address, node.Address)
+		}
+
+		if node.Port != service.Nodes[0].Port {
+			t.Fatalf("Expected node port %s got %s", service.Nodes[0].Port, node.Port)
 		}
 	}
 
