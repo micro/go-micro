7a5a9013ef4a34135603c56e0a53f30dc35afe03
Asim <asim@chuhnk.me>
2016-05-26 20:43:05 +0100

    move the option

--- server/handler.go
+++ server/handler.go
@@ -42,20 +42,20 @@ type SubscriberOptions struct {
 	Internal bool
 }
 
+// EndpointMetadata is a Handler option that allows metadata to be added to
+// individual endpoints.
+func EndpointMetadata(name string, md map[string]string) HandlerOption {
+	return func(o *HandlerOptions) {
+		o.Metadata[name] = md
+	}
+}
+
 // Internal Handler options specifies that a handler is not advertised
 // to the discovery system. In the future this may also limit request
 // to the internal network or authorised user.
 func InternalHandler(b bool) HandlerOption {
 	return func(o *HandlerOptions) {
 		o.Internal = b
-	}
-}
-
-// EndpointMetadata is a Handler option that allows metadata to be added to
-// individual endpoints.
-func EndpointMetadata(name string, md map[string]string) HandlerOption {
-	return func(o *HandlerOptions) {
-		o.Metadata[name] = md
 	}
 }
 
