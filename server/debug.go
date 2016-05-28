93c1ae6221dae5837cefd12537e88e5551a957c4
Asim <asim@chuhnk.me>
2016-05-28 22:30:47 +0100

    Add debug stats handler

--- server/debug.go
+++ server/debug.go
@@ -2,18 +2,11 @@ package server
 
 import (
 	"github.com/micro/go-micro/server/debug"
-	proto "github.com/micro/go-micro/server/debug/proto"
-
-	"golang.org/x/net/context"
 )
 
 // We use this to wrap any debug handlers so we preserve the signature Debug.{Method}
 type Debug struct {
 	debug.DebugHandler
-}
-
-func (d *Debug) Health(ctx context.Context, req *proto.HealthRequest, rsp *proto.HealthResponse) error {
-	return d.DebugHandler.Health(ctx, req, rsp)
 }
 
 func registerDebugHandler(s Server) {
