aeae2499a0279e5e104a82974b2bb10e7611e103
chriss <chriss@vimeo.com>
2016-11-18 16:41:39 +0000

    Expose configuration options for consul

--- registry/consul/options.go
+++ registry/consul/options.go
@@ -1,6 +1,9 @@
 package consul
 
 import (
+	"net/http"
+	"time"
+
 	consul "github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/registry"
 	"golang.org/x/net/context"
@@ -12,6 +15,60 @@ func Config(c *consul.Config) registry.Option {
 			o.Context = context.Background()
 		}
 		o.Context = context.WithValue(o.Context, "consul_config", c)
+	}
+}
+
+func Address(a string) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_address", a)
+	}
+}
+
+func Scheme(s string) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_scheme", s)
+	}
+}
+
+func Datacenter(d string) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_datacenter", d)
+	}
+}
+
+func HttpClient(c *http.Client) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_http-client", c)
+	}
+}
+
+func HttpAuth(a *consul.HttpBasicAuth) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_http-auth", a)
+	}
+}
+
+func WaitTime(t time.Duration) registry.Option {
+	return func(o *registry.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, "consul_wait-time", t)
 	}
 }
 
