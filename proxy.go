1868a16159cb6054e9c9e8c3d686fc0245465b9a
Asim Aslam <asim@chuhnk.me>
2018-08-14 17:35:44 +0100

    first simplistic version

--- proxy.go
+++ proxy.go
@@ -1,21 +1,52 @@
 package micro
 
 import (
+	"github.com/hashicorp/consul/api"
+	"github.com/hashicorp/consul/connect"
 	"github.com/micro/go-micro/registry/consul"
 	"github.com/micro/go-micro/transport"
-	"github.com/micro/go-micro/transport/http"
 )
 
 type proxy struct{}
+
+type proxyService struct {
+	Service
+}
 
 func newProxy(opts ...Option) Proxy {
 	return &proxy{}
 }
 
 func (p *proxy) Service(opts ...Option) Service {
-	return newService(opts...)
+	r := consul.NewRegistry(
+		consul.Connect(),
+	)
+	newOpts := append([]Option{Registry(r)}, opts...)
+	return &proxyService{newService(newOpts...)}
 }
 
 func (p *proxy) String() string {
 	return "micro"
+}
+
+func (p *proxyService) Run() error {
+	client, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		return err
+	}
+
+	// create connect service
+	svc, err := connect.NewService(p.Service.Server().Options().Name, client)
+	if err != nil {
+		return err
+	}
+	defer svc.Close()
+
+	// setup connect tls config
+	p.Service.Options().Transport.Init(
+		transport.TLSConfig(svc.ServerTLSConfig()),
+	)
+
+	// run service
+	return p.Service.Run()
 }
