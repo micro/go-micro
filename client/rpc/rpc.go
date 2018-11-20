172ffee8c38d915f2a4198d9202658a4c0765d16
Asim Aslam <asim@aslam.me>
2018-11-20 10:30:53 +0000

    add rpc package comments

--- client/rpc/rpc.go
+++ client/rpc/rpc.go
@@ -1,9 +1,11 @@
+// Package rpc provides an rpc client
 package rpc
 
 import (
 	"github.com/micro/go-micro/client"
 )
 
+// NewClient returns a new micro client interface
 func NewClient(opts ...client.Option) client.Client {
 	return client.NewClient(opts...)
 }
