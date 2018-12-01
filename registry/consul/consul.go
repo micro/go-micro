a1665ab37a56634bd9838a5ef26ef69b9b05e9b8
Asim Aslam <asim@aslam.me>
2018-12-01 12:54:46 +0000

    Add consul package comment

--- registry/consul/consul.go
+++ registry/consul/consul.go
@@ -1,9 +1,11 @@
+// Package consul provides a consul based registry and is the default discovery system
 package consul
 
 import (
 	"github.com/micro/go-micro/registry"
 )
 
+// NewRegistry returns a new consul registry
 func NewRegistry(opts ...registry.Option) registry.Registry {
 	return registry.NewRegistry(opts...)
 }
