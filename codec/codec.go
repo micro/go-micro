f853f88bcd8c5d7ea588be0c98a8b02f1dcc222d
Asim Aslam <asim@aslam.me>
2019-01-10 20:35:20 +0000

    gofmt

--- codec/codec.go
+++ codec/codec.go
@@ -38,8 +38,7 @@ type Writer interface {
 	Write(*Message, interface{}) error
 }
 
-
-// Marshaler is a simple encoding interface used for the broker/transport 
+// Marshaler is a simple encoding interface used for the broker/transport
 // where headers are not supported by the underlying implementation.
 type Marshaler interface {
 	Marshal(interface{}) ([]byte, error)
