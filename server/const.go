590c33948d284a905e691349d01c9702623b1577
shuxian <printfcoder@gmail.com>
2019-03-24 16:52:22 +0800

    service address metadata

---
+++ server/const.go
@@ -0,0 +1,9 @@
+package server
+
+const (
+	MetadataFieldNameServerType     = "server_type"
+	MetadataFieldNameServerAddress  = "server_address"
+	MetadataFieldNameServerName     = "server_name"
+	MetadataServiceTypeAPIGateway   = "api-gateway"
+	MetadataServiceTypeWebDashboard = "web-dashboard"
+)
