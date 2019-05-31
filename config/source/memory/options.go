082d0b9f05fe8fcb637893c5a9eed29f105e47a5
Asim Aslam <asim@aslam.me>
2019-05-31 13:44:28 +0100

    add memory config source options for json/yaml

--- config/source/memory/options.go
+++ config/source/memory/options.go
@@ -3,10 +3,22 @@ package memory
 import (
 	"context"
 
-	"github.com/micro/go-micro/config/source"
+	"github.com/micro/go-config/source"
 )
 
 type changeSetKey struct{}
+
+func withData(d []byte, f string) source.Option {
+	return func(o *source.Options) {
+		if o.Context == nil {
+			o.Context = context.Background()
+		}
+		o.Context = context.WithValue(o.Context, changeSetKey{}, &source.ChangeSet{
+			Data:   d,
+			Format: f,
+		})
+	}
+}
 
 // WithChangeSet allows a changeset to be set
 func WithChangeSet(cs *source.ChangeSet) source.Option {
@@ -18,15 +30,12 @@ func WithChangeSet(cs *source.ChangeSet) source.Option {
 	}
 }
 
-// WithData allows the source data to be set
-func WithData(d []byte) source.Option {
-	return func(o *source.Options) {
-		if o.Context == nil {
-			o.Context = context.Background()
-		}
-		o.Context = context.WithValue(o.Context, changeSetKey{}, &source.ChangeSet{
-			Data:   d,
-			Format: "json",
-		})
-	}
+// WithJson allows the source data to be set to json
+func WithJson(d []byte) source.Option {
+	return withData(d, "json")
+}
+
+// WithYaml allows the source data to be set to yaml
+func WithYaml(d []byte) source.Option {
+	return withData(d, "yaml")
 }
