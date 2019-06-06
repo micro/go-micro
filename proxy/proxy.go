c60b5a45bbae592d737d3b7504abbdcd5e65181f
Asim Aslam <asim@aslam.me>
2019-06-06 17:49:41 +0100

    Add proxy interface

--- proxy/proxy.go
+++ proxy/proxy.go
@@ -1,3 +1,18 @@
 // Package proxy is a transparent proxy built on the go-micro/server
 package proxy
 
+import (
+	"context"
+
+	"github.com/micro/go-micro/init"
+	"github.com/micro/go-micro/server"
+)
+
+// Proxy can be used as a proxy server for go-micro services
+type Proxy interface {
+	init.Options
+	// ServeRequest will serve a request
+	ServeRequest(context.Context, Request, Response) error
+	// run the proxy
+	Run() error
+}
