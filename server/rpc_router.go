f5ac23823107fb1356060ffe00069b920b4190e4
Asim Aslam <asim@aslam.me>
2019-06-07 15:15:22 +0100

    Include the decoded body

--- server/rpc_router.go
+++ server/rpc_router.go
@@ -188,6 +188,7 @@ func (s *service) call(ctx context.Context, router *router, sending *sync.Mutex,
 		method:      req.msg.Method,
 		endpoint:    req.msg.Endpoint,
 		body:        req.msg.Body,
+		rawBody:     argv.Interface(),
 	}
 
 	if !mtype.stream {
