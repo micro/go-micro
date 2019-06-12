af096951fc335d712aa1d9f79dd0d12110b8f211
Asim Aslam <asim@aslam.me>
2019-06-12 12:54:45 +0100

    update import names for mucp

--- service/mucp/mucp.go
+++ service/mucp/mucp.go
@@ -4,15 +4,15 @@ package mucp
 import (
 	// TODO: change to go-micro/service
 	"github.com/micro/go-micro"
-	"github.com/micro/go-micro/client/mucp"
-	"github.com/micro/go-micro/server/mucp"
+	cmucp "github.com/micro/go-micro/client/mucp"
+	smucp "github.com/micro/go-micro/server/mucp"
 )
 
 // NewService returns a new mucp service
 func NewService(opts ...micro.Option) micro.Service {
 	options := []micro.Option{
-		micro.Client(mucp.NewClient()),
-		micro.Server(mucp.NewServer()),
+		micro.Client(cmucp.NewClient()),
+		micro.Server(smucp.NewServer()),
 	}
 
 	options = append(options, opts...)
