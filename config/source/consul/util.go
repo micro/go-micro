92b998c3abdd041bb7923019e3b739045e06d1ea
magodo <wztdyl@sina.com>
2019-06-21 00:25:39 +0800

    consul config source support 1st-level array
    Check whetehr the 1st level encoded json is array or not, to
    support 1st level array in consul config.
    
    During debug, i suspected the incapability of arrray is caused by
    json reader, so i added test for array. I think it makes no harm
    to also check that in.

--- config/source/consul/util.go
+++ config/source/consul/util.go
@@ -8,40 +8,78 @@ import (
 	"github.com/micro/go-micro/config/encoder"
 )
 
+type jsonValue interface {
+	Value() interface{}
+	Decode(encoder.Encoder, []byte) (jsonValue, error)
+}
+type jsonArrayValue []interface{}
+type jsonMapValue map[string]interface{}
+
+func (a jsonArrayValue) Value() interface{} { return a }
+func (a jsonArrayValue) Decode(e encoder.Encoder, b []byte) (jsonValue, error) {
+	v := jsonArrayValue{}
+	err := e.Decode(b, &v)
+	return v, err
+}
+func (m jsonMapValue) Value() interface{} { return m }
+func (m jsonMapValue) Decode(e encoder.Encoder, b []byte) (jsonValue, error) {
+	v := jsonMapValue{}
+	err := e.Decode(b, &v)
+	return v, err
+}
+
 func makeMap(e encoder.Encoder, kv api.KVPairs, stripPrefix string) (map[string]interface{}, error) {
+
 	data := make(map[string]interface{})
 
 	// consul guarantees lexicographic order, so no need to sort
 	for _, v := range kv {
 		pathString := strings.TrimPrefix(strings.TrimPrefix(v.Key, stripPrefix), "/")
-		var val map[string]interface{}
+		if pathString == "" {
+			continue
+		}
+		var val jsonValue
+		var err error
 
 		// ensure a valid value is stored at this location
 		if len(v.Value) > 0 {
-			if err := e.Decode(v.Value, &val); err != nil {
-				return nil, fmt.Errorf("faild decode value. path: %s, error: %s", pathString, err)
-			}
-		}
-
-		// set target at the root
-		target := data
-
-		// then descend to the target location, creating as we go, if need be
-		if pathString != "" {
-			path := strings.Split(pathString, "/")
-			// find (or create) the location we want to put this value at
-			for _, dir := range path {
-				if _, ok := target[dir]; !ok {
-					target[dir] = make(map[string]interface{})
+			// check whether this is an array
+			if v.Value[0] == 91 && v.Value[len(v.Value)-1] == 93 {
+				val = jsonArrayValue{}
+				if val, err = val.Decode(e, v.Value); err != nil {
+					return nil, fmt.Errorf("faild decode value. path: %s, error: %s", pathString, err)
 				}
-				target = target[dir].(map[string]interface{})
+			} else {
+				val = jsonMapValue{}
+				if val, err = val.Decode(e, v.Value); err != nil {
+					return nil, fmt.Errorf("faild decode value. path: %s, error: %s", pathString, err)
+				}
 			}
-
 		}
+
+		target := data
+		path := strings.Split(pathString, "/")
+		// find (or create) the leaf node we want to put this value at
+		for _, dir := range path[:len(path)-1] {
+			if _, ok := target[dir]; !ok {
+				target[dir] = make(map[string]interface{})
+			}
+			target = target[dir].(map[string]interface{})
+		}
+
+		leafDir := path[len(path)-1]
 
 		// copy over the keys from the value
-		for k := range val {
-			target[k] = val[k]
+		switch val.(type) {
+		case jsonArrayValue:
+			target[leafDir] = val.Value()
+		case jsonMapValue:
+			target[leafDir] = make(map[string]interface{})
+			target = target[leafDir].(map[string]interface{})
+			mapv := val.Value().(jsonMapValue)
+			for k := range mapv {
+				target[k] = mapv[k]
+			}
 		}
 	}
 
