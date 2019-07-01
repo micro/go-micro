67b893a32ebde83270a1d2a32f66af5d6fd3bcf7
Asim Aslam <asim@aslam.me>
2019-07-01 08:34:51 +0100

    update stats interface

--- debug/stats/stats.go
+++ debug/stats/stats.go
@@ -1,4 +1,25 @@
 // Package stats provides metrics
 package stats
 
-type Stats interface{}
+import (
+	"time"
+)
+
+type Stats interface {
+	// Get the stats
+	Get(id string) (*Metric, error)
+	// Record a stat
+	Record(id string, m *Metric) error
+	// History of metric
+	History(id string) ([]*Metric, error)
+}
+
+// A single stat
+type Metric struct {
+	// Id of the metric
+	Id string
+	// Time of recording
+	Timestamp time.Time
+	// the metric values
+	Values map[string]interface{}
+}
