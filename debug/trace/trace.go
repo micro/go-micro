321a31817888beb590be2188fe000cbbbf04ce5d
Asim Aslam <asim@aslam.me>
2019-07-01 08:53:58 +0100

    Update trace interface

--- debug/trace/trace.go
+++ debug/trace/trace.go
@@ -1,4 +1,34 @@
 // Package trace is for stack tracing
 package trace
 
-type Trace interface{}
+import (
+	"time"
+)
+
+// Trace provides stack tracing recording
+type Trace interface {
+	// return a stack trace
+	Get(id string) (*Stack, error)
+	// Record against a stack
+	Record(id string, f *Frame) error
+}
+
+// Stack is a stack trace
+type Stack struct {
+	// Id of the stack
+	Id string
+	// Frames for the stack trace
+	Frames []*Frame
+}
+
+// Frame is a frame in the stack trace
+type Frame struct {
+	// Id of this frame
+	Id string
+	// Time of the recording
+	Timestamp time.Time
+	// the frame context
+	Context map[string]interface{}
+	// the frame data
+	Data []byte
+}
