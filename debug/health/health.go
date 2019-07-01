ce318b675a2d6b0c645b0619c5e9134fb26fdba2
Asim Aslam <asim@aslam.me>
2019-07-01 08:29:58 +0100

    Update health debug interface

--- debug/health/health.go
+++ debug/health/health.go
@@ -1,6 +1,10 @@
 // Package health provides health info
 package health
 
+import (
+	"time"
+)
+
 const (
 	CheckOK Code = iota
 	CheckFailed
@@ -8,21 +12,29 @@ const (
 
 type Code int
 
+// Health manages the health of services
 type Health interface {
-	Register(*Check) error
-	Check(id string) (*Status, error)
+	// Register a healthcheck
+	Register(id string, c *Check) error
+	// Check the health
+	Check(id string) (*Result, error)
+	// Get healthcheck history
+	History(id string) ([]*Result, error)
+	// Notify of check results
+	Notify() (<-chan *Result, error)
 }
 
-// Check performs some healthcheck
-type Check struct {
-	// Id of the healthcheck
+// Check is a healthcheck function
+type Check func() error
+
+// Result is the result of a healthcheck
+type Result struct {
+	// check id
 	Id string
-	// The function to execute
-	Exec func() error
-}
-
-// Status of a healthcheck
-type Status struct {
+	// time of status check
+	Timestamp time.Time
+	// the status code
 	Code Code
+	// info message
 	Info string
 }
