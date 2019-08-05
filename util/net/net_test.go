fab02412acf9f352323697c75f595d19a5e9bc9d
Alexandr Ivanchenko <trim.lexx@gmail.com>
2019-08-05 15:20:13 +0300

    Net addr: check for port absence added

--- util/net/net_test.go
+++ util/net/net_test.go
@@ -18,4 +18,9 @@ func TestListen(t *testing.T) {
 		}
 		defer l.Close()
 	}
+
+	// TODO nats case test
+	// natsAddr := "_INBOX.bID2CMRvlNp0vt4tgNBHWf"
+	// Expect addr DO NOT has extra ":" at the end!
+
 }
