45d8d5cf4fadbe1109f16b9d54acf28e1e1348fe
Jake Sanders <jake@micro.mu>
2019-10-09 16:35:33 +0100

    One more Library -> Provider

--- api/server/acme/autocert/autocert.go
+++ api/server/acme/autocert/autocert.go
@@ -11,12 +11,12 @@ import (
 // autoCertACME is the ACME provider from golang.org/x/crypto/acme/autocert
 type autocertProvider struct{}
 
-// NewListener implements acme.Library
+// NewListener implements acme.Provider
 func (a *autocertProvider) NewListener(ACMEHosts ...string) (net.Listener, error) {
 	return autocert.NewListener(ACMEHosts...), nil
 }
 
-// New returns an autocert acme.Library
+// New returns an autocert acme.Provider
 func New() acme.Provider {
 	return &autocertProvider{}
 }
