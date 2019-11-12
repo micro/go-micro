92c25c6c2398fa9fd227d32616143fc853ab1187
Milos Gajdos <milosgajdos83@gmail.com>
2019-11-12 20:12:52 +0000

    Ripped out existing code in preparation to small rework

--- runtime/kubernetes/kubernetes.go
+++ runtime/kubernetes/kubernetes.go
@@ -2,10 +2,8 @@
 package kubernetes
 
 import (
-	"errors"
 	"fmt"
 	"strconv"
-	"strings"
 	"sync"
 	"time"
 
@@ -24,8 +22,6 @@ type kubernetes struct {
 	start chan *runtime.Service
 	// used to stop the runtime
 	closed chan bool
-	// service tracks deployed services
-	services map[string]*runtime.Service
 	// client is kubernetes client
 	client client.Kubernetes
 }
@@ -44,11 +40,10 @@ func NewRuntime(opts ...runtime.Option) runtime.Runtime {
 	client := client.NewClientInCluster()
 
 	return &kubernetes{
-		options:  options,
-		closed:   make(chan bool),
-		start:    make(chan *runtime.Service, 128),
-		services: make(map[string]*runtime.Service),
-		client:   client,
+		options: options,
+		closed:  make(chan bool),
+		start:   make(chan *runtime.Service, 128),
+		client:  client,
 	}
 }
 
@@ -69,28 +64,13 @@ func (k *kubernetes) Create(s *runtime.Service, opts ...runtime.CreateOption) er
 	k.Lock()
 	defer k.Unlock()
 
-	// TODO:
-	// * create service
-	// * create deployment
-
-	// NOTE: our services have micro- prefix
-	muName := strings.Split(s.Name, ".")
-	s.Name = "micro-" + muName[len(muName)-1]
-
-	// NOTE: we are tracking this in memory for now
-	if _, ok := k.services[s.Name]; ok {
-		return errors.New("service already registered")
-	}
-
 	var options runtime.CreateOptions
 	for _, o := range opts {
 		o(&options)
 	}
 
-	// save service
-	k.services[s.Name] = s
 	// push into start queue
-	k.start <- k.services[s.Name]
+	k.start <- s
 
 	return nil
 }
@@ -133,12 +113,6 @@ func (k *kubernetes) Delete(s *runtime.Service) error {
 	// TODO:
 	// * delete service
 	// * delete dpeloyment
-
-	// NOTE: we are tracking this in memory for now
-	if s, ok := k.services[s.Name]; ok {
-		delete(k.services, s.Name)
-		return nil
-	}
 
 	return nil
 }
@@ -209,49 +183,11 @@ func (k *kubernetes) run(events <-chan runtime.Event) {
 					continue
 				}
 				buildTime := time.Unix(updateTimeStamp, 0)
-				processEvent := func(event runtime.Event, service *runtime.Service) error {
-					buildTimeStamp, err := strconv.ParseInt(service.Version, 10, 64)
-					if err != nil {
-						return err
-					}
-					muBuild := time.Unix(buildTimeStamp, 0)
-					if buildTime.After(muBuild) {
-						version := fmt.Sprintf("%d", buildTime.Unix())
-						muService := &runtime.Service{
-							Name:    service.Name,
-							Source:  service.Source,
-							Path:    service.Path,
-							Exec:    service.Exec,
-							Version: version,
-						}
-						if err := k.Update(muService); err != nil {
-							return err
-						}
-						service.Version = version
-					}
-					return nil
-				}
-				k.Lock()
+				log.Debugf("build time: %s", buildTime)
 				if len(event.Service) > 0 {
-					service, ok := k.services[event.Service]
-					if !ok {
-						log.Debugf("Runtime unknown service: %s", event.Service)
-						k.Unlock()
-						continue
-					}
-					if err := processEvent(event, service); err != nil {
-						log.Debugf("Runtime error updating service %s: %v", event.Service, err)
-					}
-					k.Unlock()
+					// TODO:
 					continue
 				}
-				// if blank service was received we update all services
-				for _, service := range k.services {
-					if err := processEvent(event, service); err != nil {
-						log.Debugf("Runtime error updating service %s: %v", service.Name, err)
-					}
-				}
-				k.Unlock()
 			}
 		case <-k.closed:
 			log.Debugf("Runtime stopped")
