bc82d14303e66d789b8a685387bbfc1773ea39a0
Ben Toogood <ben@micro.mu>
2020-01-30 10:46:14 +0000

    Update Auth Interface

--- auth/auth.go
+++ auth/auth.go
@@ -7,34 +7,40 @@ import (
 
 // Auth providers authentication and authorization
 type Auth interface {
-	// Generate a new auth token
-	Generate(string) (*Token, error)
-	// Revoke an authorization token
-	Revoke(*Token) error
-	// Grant access to a resource
-	Grant(*Token, *Service) error
-	// Verify a token can access a resource
-	Verify(*Token, *Service) error
+	// Generate a new auth ServiceAccount
+	Generate(string) (*ServiceAccount, error)
+	// Revoke an authorization ServiceAccount
+	Revoke(*ServiceAccount) error
+	// AddRole to the service account
+	AddRole(*ServiceAccount, *Role) error
+	// RemoveRole from a service account
+	RemoveRole(*ServiceAccount, *Role) error
 }
 
-// Service is some thing to provide access to
-type Service struct {
+// Resource is some thing to provide access to
+type Resource struct {
 	// Name of the resource
 	Name string
-	// Endpoint is the specific endpoint
-	Endpoint string
+	// Type of resource, e.g. Service
+	Type string
 }
 
-// Token providers by an auth provider
-type Token struct {
-	// Unique token id
+// Role a service account has
+type Role struct {
+	Name     string
+	Resource *Resource
+}
+
+// ServiceAccount providers by an auth provider
+type ServiceAccount struct {
+	// Unique ServiceAccount id
 	Id string `json: "id"`
-	// Time of token creation
+	// Time of ServiceAccount creation
 	Created time.Time `json:"created"`
-	// Time of token expiry
+	// Time of ServiceAccount expiry
 	Expiry time.Time `json:"expiry"`
-	// Roles associated with the token
-	Roles []string `json:"roles"`
+	// Roles associated with the ServiceAccount
+	Roles []*Role `json:"roles"`
 	// Any other associated metadata
 	Metadata map[string]string `json:"metadata"`
 }
