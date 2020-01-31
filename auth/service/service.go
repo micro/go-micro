d2b44a100f8b2989f1946ecc5692a2c2b510425d
Ben Toogood <ben@micro.mu>
2020-01-31 09:56:46 +0000

    Refactor serializing and deserializing service accounts

--- auth/service/service.go
+++ auth/service/service.go
@@ -9,6 +9,20 @@ import (
 	"github.com/micro/go-micro/client"
 )
 
+// NewAuth returns a new instance of the Auth service
+func NewAuth(opts ...auth.Option) auth.Auth {
+	options := auth.Options{}
+
+	for _, o := range opts {
+		o(&options)
+	}
+
+	client := client.DefaultClient
+	srv := pb.NewAuthService("go.micro.srv.auth", client)
+
+	return &svc{options, srv}
+}
+
 // svc is the implementation of the Auth interface
 type svc struct {
 	options auth.Options
@@ -17,32 +31,8 @@ type svc struct {
 
 // Generate a new auth ServiceAccount
 func (s *svc) Generate(sa *auth.ServiceAccount) (*auth.ServiceAccount, error) {
-	// format the roles and resources
-	roles := make([]*pb.Role, len(sa.Roles))
-	for i, r := range sa.Roles {
-		roles[i] = &pb.Role{
-			Name: r.Name,
-		}
-
-		if r.Resource != nil {
-			roles[i].Resource = &pb.Resource{
-				Id:   r.Resource.Id,
-				Type: r.Resource.Type,
-			}
-		}
-	}
-
 	// construct the request
-	req := &pb.GenerateRequest{
-		ServiceAccount: &pb.ServiceAccount{
-			Roles:    roles,
-			Metadata: sa.Metadata,
-			Parent: &pb.Resource{
-				Id:   sa.Parent.Id,
-				Type: sa.Parent.Type,
-			},
-		},
-	}
+	req := &pb.GenerateRequest{ServiceAccount: serializeServiceAccount(sa)}
 
 	// execute the request
 	resp, err := s.auth.Generate(context.Background(), req)
@@ -51,34 +41,7 @@ func (s *svc) Generate(sa *auth.ServiceAccount) (*auth.ServiceAccount, error) {
 	}
 
 	// format the response
-	sa = &auth.ServiceAccount{
-		Token:    resp.ServiceAccount.Token,
-		Created:  time.Unix(resp.ServiceAccount.Created, 0),
-		Expiry:   time.Unix(resp.ServiceAccount.Expiry, 0),
-		Metadata: resp.ServiceAccount.Metadata,
-	}
-	if resp.ServiceAccount.Parent != nil {
-		sa.Parent = &auth.Resource{
-			Id:   resp.ServiceAccount.Parent.Id,
-			Type: resp.ServiceAccount.Parent.Type,
-		}
-	}
-
-	sa.Roles = make([]*auth.Role, len(resp.ServiceAccount.Roles))
-	for i, r := range resp.ServiceAccount.Roles {
-		sa.Roles[i] = &auth.Role{
-			Name: r.Name,
-		}
-
-		if r.Resource != nil {
-			sa.Roles[i].Resource = &auth.Resource{
-				Id:   r.Resource.Id,
-				Type: r.Resource.Type,
-			}
-		}
-	}
-
-	return sa, nil
+	return deserializeServiceAccount(resp.ServiceAccount), nil
 }
 
 // Revoke an authorization ServiceAccount
@@ -93,19 +56,67 @@ func (s *svc) Revoke(sa *auth.ServiceAccount) error {
 
 // Validate a service account token
 func (s *svc) Validate(token string) (*auth.ServiceAccount, error) {
-	return nil, nil
-}
-
-// NewAuth returns a new instance of the Auth service
-func NewAuth(opts ...auth.Option) auth.Auth {
-	options := auth.Options{}
-
-	for _, o := range opts {
-		o(&options)
+	resp, err := s.auth.Validate(context.Background(), &pb.ValidateRequest{Token: token})
+	if err != nil {
+		return nil, err
 	}
 
-	client := client.DefaultClient
-	srv := pb.NewAuthService("go.micro.srv.auth", client)
+	return deserializeServiceAccount(resp.ServiceAccount), nil
+}
 
-	return &svc{options, srv}
+func serializeServiceAccount(sa *auth.ServiceAccount) *pb.ServiceAccount {
+	roles := make([]*pb.Role, len(sa.Roles))
+	for i, r := range sa.Roles {
+		roles[i] = &pb.Role{
+			Name: r.Name,
+		}
+
+		if r.Resource != nil {
+			roles[i].Resource = &pb.Resource{
+				Id:   r.Resource.Id,
+				Type: r.Resource.Type,
+			}
+		}
+	}
+
+	return &pb.ServiceAccount{
+		Roles:    roles,
+		Metadata: sa.Metadata,
+		Parent: &pb.Resource{
+			Id:   sa.Parent.Id,
+			Type: sa.Parent.Type,
+		},
+	}
+}
+
+func deserializeServiceAccount(a *pb.ServiceAccount) *auth.ServiceAccount {
+	// format the response
+	sa := &auth.ServiceAccount{
+		Token:    a.Token,
+		Created:  time.Unix(a.Created, 0),
+		Expiry:   time.Unix(a.Expiry, 0),
+		Metadata: a.Metadata,
+	}
+	if a.Parent != nil {
+		sa.Parent = &auth.Resource{
+			Id:   a.Parent.Id,
+			Type: a.Parent.Type,
+		}
+	}
+
+	sa.Roles = make([]*auth.Role, len(a.Roles))
+	for i, r := range a.Roles {
+		sa.Roles[i] = &auth.Role{
+			Name: r.Name,
+		}
+
+		if r.Resource != nil {
+			sa.Roles[i].Resource = &auth.Resource{
+				Id:   r.Resource.Id,
+				Type: r.Resource.Type,
+			}
+		}
+	}
+
+	return sa
 }
