7187894f0f4fce326af7caa6fc37bb8fdb399f98
Ben Toogood <ben@micro.mu>
2020-02-25 11:26:56 +0000

    Fix k8s commands for github

--- runtime/kubernetes/service.go
+++ runtime/kubernetes/service.go
@@ -70,7 +70,7 @@ func newService(s *runtime.Service, c runtime.CreateOptions) *service {
 	}
 
 	// specify the command to exec
-	if len(c.Command) > 0 {
+	if strings.HasPrefix(c.Source, "github.com") && len(c.Command) > 0 {
 		kdeploy.Spec.Template.PodSpec.Containers[0].Command = c.Command
 	}
 
