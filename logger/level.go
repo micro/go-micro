e3ce45495a33030f97622f877eb5bcdd5c123f0b
ben-toogood <bentoogood@gmail.com>
2020-03-09 09:23:42 +0000

    os.Exit on log.Fatal (#1316)
    * os.Exit on log.Fatal
    
    * Fix TestOptions
    
    Co-authored-by: Ben Toogood <ben@micro.mu>

--- logger/level.go
+++ logger/level.go
@@ -1,6 +1,9 @@
 package logger
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 type Level int8
 
@@ -105,8 +108,10 @@ func Errorf(template string, args ...interface{}) {
 
 func Fatal(args ...interface{}) {
 	DefaultLogger.Log(FatalLevel, args...)
+	os.Exit(1)
 }
 
 func Fatalf(template string, args ...interface{}) {
 	DefaultLogger.Logf(FatalLevel, template, args...)
+	os.Exit(1)
 }
