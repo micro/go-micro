2e355001814475d0f27b44ec08ad525ac19f7509
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-03-11 18:25:34 +0300

    more logger
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- broker/service/service.go
+++ broker/service/service.go
@@ -8,7 +8,7 @@ import (
 	"github.com/micro/go-micro/v2/broker"
 	pb "github.com/micro/go-micro/v2/broker/service/proto"
 	"github.com/micro/go-micro/v2/client"
-	log "github.com/micro/go-micro/v2/logger"
+	"github.com/micro/go-micro/v2/logger"
 )
 
 type serviceBroker struct {
@@ -45,7 +45,9 @@ func (b *serviceBroker) Options() broker.Options {
 }
 
 func (b *serviceBroker) Publish(topic string, msg *broker.Message, opts ...broker.PublishOption) error {
-	log.Debugf("Publishing to topic %s broker %v", topic, b.Addrs)
+	if logger.V(logger.DebugLevel, logger.DefaultLogger) {
+		logger.Debugf("Publishing to topic %s broker %v", topic, b.Addrs)
+	}
 	_, err := b.Client.Publish(context.TODO(), &pb.PublishRequest{
 		Topic: topic,
 		Message: &pb.Message{
@@ -61,7 +63,9 @@ func (b *serviceBroker) Subscribe(topic string, handler broker.Handler, opts ...
 	for _, o := range opts {
 		o(&options)
 	}
-	log.Debugf("Subscribing to topic %s queue %s broker %v", topic, options.Queue, b.Addrs)
+	if logger.V(logger.DebugLevel, logger.DefaultLogger) {
+		logger.Debugf("Subscribing to topic %s queue %s broker %v", topic, options.Queue, b.Addrs)
+	}
 	stream, err := b.Client.Subscribe(context.TODO(), &pb.SubscribeRequest{
 		Topic: topic,
 		Queue: options.Queue,
@@ -83,19 +87,27 @@ func (b *serviceBroker) Subscribe(topic string, handler broker.Handler, opts ...
 		for {
 			select {
 			case <-sub.closed:
-				log.Debugf("Unsubscribed from topic %s", topic)
+				if logger.V(logger.DebugTraceLevel, logger.DefaultLogger) {
+					logger.Debugf("Unsubscribed from topic %s", topic)
+				}
 				return
 			default:
-				// run the subscriber
-				log.Debugf("Streaming from broker %v to topic [%s] queue [%s]", b.Addrs, topic, options.Queue)
+				if logger.V(logger.DebugTraceLevel, logger.DefaultLogger) {
+					// run the subscriber
+					logger.Debugf("Streaming from broker %v to topic [%s] queue [%s]", b.Addrs, topic, options.Queue)
+				}
 				if err := sub.run(); err != nil {
-					log.Debugf("Resubscribing to topic %s broker %v", topic, b.Addrs)
+					if logger.V(logger.DebugTraceLevel, logger.DefaultLogger) {
+						logger.Debugf("Resubscribing to topic %s broker %v", topic, b.Addrs)
+					}
 					stream, err := b.Client.Subscribe(context.TODO(), &pb.SubscribeRequest{
 						Topic: topic,
 						Queue: options.Queue,
 					}, client.WithAddress(b.Addrs...), client.WithRequestTimeout(time.Hour))
 					if err != nil {
-						log.Debugf("Failed to resubscribe to topic %s: %v", topic, err)
+						if logger.V(logger.DebugTraceLevel, logger.DefaultLogger) {
+							logger.Debugf("Failed to resubscribe to topic %s: %v", topic, err)
+						}
 						time.Sleep(time.Second)
 						continue
 					}
