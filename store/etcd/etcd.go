2bb9acd28018c7c5ead207bad1345c19a283f2f5
Jake Sanders <jake@micro.mu>
2020-03-12 12:25:04 +0000

    Fix etcd tests

--- store/etcd/etcd.go
+++ store/etcd/etcd.go
@@ -5,6 +5,8 @@ import (
 	"bytes"
 	"context"
 	"encoding/gob"
+	"math"
+	"strings"
 	"time"
 
 	"github.com/micro/go-micro/v2/store"
@@ -86,10 +88,13 @@ func (e *etcdStore) Read(key string, opts ...store.ReadOption) ([]*store.Record,
 	for _, o := range opts {
 		o(&readOpts)
 	}
+	if readOpts.Suffix {
+		return e.readSuffix(key, readOpts)
+	}
 
 	var etcdOpts []clientv3.OpOption
 	if readOpts.Prefix {
-		etcdOpts = append(etcdOpts, clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortDescend))
+		etcdOpts = append(etcdOpts, clientv3.WithPrefix(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend))
 	}
 	resp, err := e.client.KV.Get(context.Background(), key, etcdOpts...)
 	if err != nil {
@@ -102,7 +107,7 @@ func (e *etcdStore) Read(key string, opts ...store.ReadOption) ([]*store.Record,
 	for _, kv := range resp.Kvs {
 		ir := internalRecord{}
 		if err := gob.NewDecoder(bytes.NewReader(kv.Value)).Decode(&ir); err != nil {
-			return records, errors.Wrapf(err, "couldn't decode %s into internalRecord", err.Error)
+			return records, errors.Wrapf(err, "couldn't decode %s into internalRecord", err.Error())
 		}
 		r := store.Record{
 			Key:   ir.Key,
@@ -113,6 +118,44 @@ func (e *etcdStore) Read(key string, opts ...store.ReadOption) ([]*store.Record,
 		}
 		records = append(records, &r)
 	}
+	if readOpts.Limit > 0 || readOpts.Offset > 0 {
+		return records[readOpts.Offset:min(readOpts.Limit, uint(len(records)))], nil
+	}
+	return records, nil
+}
+
+func (e *etcdStore) readSuffix(key string, readOpts store.ReadOptions) ([]*store.Record, error) {
+	opts := []store.ListOption{store.ListSuffix(key)}
+	if readOpts.Prefix {
+		opts = append(opts, store.ListPrefix(key))
+	}
+	keys, err := e.List(opts...)
+	if err != nil {
+		return nil, errors.Wrapf(err, "Couldn't list with suffix %s", key)
+	}
+	var records []*store.Record
+	for _, k := range keys {
+		resp, err := e.client.KV.Get(context.Background(), k)
+		if err != nil {
+			return nil, errors.Wrapf(err, "Couldn't get key %s", k)
+		}
+		ir := internalRecord{}
+		if err := gob.NewDecoder(bytes.NewReader(resp.Kvs[0].Value)).Decode(&ir); err != nil {
+			return records, errors.Wrapf(err, "couldn't decode %s into internalRecord", err.Error())
+		}
+		r := store.Record{
+			Key:   ir.Key,
+			Value: ir.Value,
+		}
+		if !ir.ExpiresAt.IsZero() {
+			r.Expiry = time.Until(ir.ExpiresAt)
+		}
+		records = append(records, &r)
+
+	}
+	if readOpts.Limit > 0 || readOpts.Offset > 0 {
+		return records[readOpts.Offset:min(readOpts.Limit, uint(len(records)))], nil
+	}
 	return records, nil
 }
 
@@ -122,7 +165,7 @@ func (e *etcdStore) Write(r *store.Record, opts ...store.WriteOption) error {
 		o(&options)
 	}
 
-	if len(opts > 0) {
+	if len(opts) > 0 {
 		// Copy the record before applying options, or the incoming record will be mutated
 		newRecord := store.Record{}
 		newRecord.Key = r.Key
@@ -137,9 +180,8 @@ func (e *etcdStore) Write(r *store.Record, opts ...store.WriteOption) error {
 			newRecord.Expiry = options.TTL
 		}
 		return e.write(&newRecord)
-	} else {
-		return e.write(r)
 	}
+	return e.write(r)
 }
 
 func (e *etcdStore) write(r *store.Record) error {
@@ -150,7 +192,14 @@ func (e *etcdStore) write(r *store.Record) error {
 	copy(ir.Value, r.Value)
 	if r.Expiry != 0 {
 		ir.ExpiresAt = time.Now().Add(r.Expiry)
-		lr, err := e.client.Lease.Grant(context.Background(), r.Expiry.Nanoseconds())
+		var leasexpiry int64
+		if r.Expiry.Seconds() < 5.0 {
+			// minimum etcd lease is 5 seconds
+			leasexpiry = 5
+		} else {
+			leasexpiry = int64(math.Ceil(r.Expiry.Seconds()))
+		}
+		lr, err := e.client.Lease.Grant(context.Background(), leasexpiry)
 		if err != nil {
 			return errors.Wrapf(err, "couldn't grant an etcd lease for %s", r.Key)
 		}
@@ -165,15 +214,55 @@ func (e *etcdStore) write(r *store.Record) error {
 }
 
 func (e *etcdStore) Delete(key string, opts ...store.DeleteOption) error {
-	return nil
+	options := store.DeleteOptions{}
+	for _, o := range opts {
+		o(&options)
+	}
+	_, err := e.client.KV.Delete(context.Background(), key)
+	return errors.Wrapf(err, "couldn't delete key %s", key)
 }
 
 func (e *etcdStore) List(opts ...store.ListOption) ([]string, error) {
-	return nil, nil
+	options := store.ListOptions{}
+	for _, o := range opts {
+		o(&options)
+	}
+	searchPrefix := ""
+	if len(options.Prefix) > 0 {
+		searchPrefix = options.Prefix
+	}
+	resp, err := e.client.KV.Get(context.Background(), searchPrefix, clientv3.WithPrefix(), clientv3.WithKeysOnly(), clientv3.WithSort(clientv3.SortByKey, clientv3.SortAscend))
+	if err != nil {
+		return nil, errors.Wrap(err, "couldn't list, etcd get failed")
+	}
+	if len(options.Suffix) == 0 {
+		keys := make([]string, resp.Count)
+		for i, kv := range resp.Kvs {
+			keys[i] = string(kv.Key)
+		}
+		return keys, nil
+	}
+	keys := []string{}
+	for _, kv := range resp.Kvs {
+		if strings.HasSuffix(string(kv.Key), options.Suffix) {
+			keys = append(keys, string(kv.Key))
+		}
+	}
+	if options.Limit > 0 || options.Offset > 0 {
+		return keys[options.Offset:min(options.Limit, uint(len(keys)))], nil
+	}
+	return keys, nil
 }
 
 type internalRecord struct {
 	Key       string
 	Value     []byte
 	ExpiresAt time.Time
+}
+
+func min(i, j uint) uint {
+	if i < j {
+		return i
+	}
+	return j
 }
