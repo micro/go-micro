ef4921f41d68cca9c8274137234b7caba2ece075
Jake <i@am.so-aweso.me>
2020-03-17 16:09:26 +0000

    Add prefix, suffix, limit, offset for cockroachdb store

--- store/cockroach/cockroach.go
+++ store/cockroach/cockroach.go
@@ -28,10 +28,12 @@ type sqlStore struct {
 	database string
 	table    string
 
-	list    *sql.Stmt
-	readOne *sql.Stmt
-	write   *sql.Stmt
-	delete  *sql.Stmt
+	list       *sql.Stmt
+	readOne    *sql.Stmt
+	readMany   *sql.Stmt
+	readOffset *sql.Stmt
+	write      *sql.Stmt
+	delete     *sql.Stmt
 
 	options store.Options
 }
@@ -92,7 +94,9 @@ func (s *sqlStore) Read(key string, opts ...store.ReadOption) ([]*store.Record, 
 		o(&options)
 	}
 
-	// TODO: make use of options.Prefix using WHERE key LIKE = ?
+	if options.Prefix || options.Suffix {
+		return s.read(key, options)
+	}
 
 	var records []*store.Record
 	var timehelper pq.NullTime
@@ -115,6 +119,61 @@ func (s *sqlStore) Read(key string, opts ...store.ReadOption) ([]*store.Record, 
 		records = append(records, record)
 	} else {
 		records = append(records, record)
+	}
+
+	return records, nil
+}
+
+// Read Many records
+func (s *sqlStore) read(key string, options store.ReadOptions) ([]*store.Record, error) {
+	pattern := "%"
+	if options.Prefix {
+		pattern = key + pattern
+	}
+	if options.Suffix {
+		pattern = pattern + key
+	}
+	var rows *sql.Rows
+	var err error
+	if options.Limit != 0 {
+		rows, err = s.readOffset.Query(pattern, options.Limit, options.Offset)
+	} else {
+		rows, err = s.readMany.Query(pattern)
+	}
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return []*store.Record{}, nil
+		}
+		return []*store.Record{}, errors.Wrap(err, "sqlStore.read failed")
+	}
+	defer rows.Close()
+	var records []*store.Record
+	var timehelper pq.NullTime
+
+	for rows.Next() {
+		record := &store.Record{}
+		if err := rows.Scan(&record.Key, &record.Value, &timehelper); err != nil {
+			return records, err
+		}
+		if timehelper.Valid {
+			if timehelper.Time.Before(time.Now()) {
+				// record has expired
+				go s.Delete(record.Key)
+			} else {
+				record.Expiry = time.Until(timehelper.Time)
+				records = append(records, record)
+			}
+		} else {
+			records = append(records, record)
+		}
+	}
+	rowErr := rows.Close()
+	if rowErr != nil {
+		// transaction rollback or something
+		return records, rowErr
+	}
+	if err := rows.Err(); err != nil {
+		return records, err
 	}
 
 	return records, nil
@@ -174,16 +233,44 @@ func (s *sqlStore) initDB() error {
 		return errors.Wrap(err, "Couldn't create table")
 	}
 
+	// Create Index
+	_, err = s.db.Exec(fmt.Sprintf(`CREATE INDEX IF NOT EXISTS "%s" ON %s.%s USING btree ("key")`, "key_index_"+s.table, s.database, s.table))
+	if err != nil {
+		return err
+	}
+
 	list, err := s.db.Prepare(fmt.Sprintf("SELECT key, value, expiry FROM %s.%s;", s.database, s.table))
 	if err != nil {
 		return errors.Wrap(err, "List statement couldn't be prepared")
+	}
+	if s.list != nil {
+		s.list.Close()
 	}
 	s.list = list
 	readOne, err := s.db.Prepare(fmt.Sprintf("SELECT key, value, expiry FROM %s.%s WHERE key = $1;", s.database, s.table))
 	if err != nil {
 		return errors.Wrap(err, "ReadOne statement couldn't be prepared")
 	}
+	if s.readOne != nil {
+		s.readOne.Close()
+	}
 	s.readOne = readOne
+	readMany, err := s.db.Prepare(fmt.Sprintf("SELECT key, value, expiry FROM %s.%s WHERE key LIKE $1;", s.database, s.table))
+	if err != nil {
+		return errors.Wrap(err, "ReadMany statement couldn't be prepared")
+	}
+	if s.readMany != nil {
+		s.readMany.Close()
+	}
+	s.readMany = readMany
+	readOffset, err := s.db.Prepare(fmt.Sprintf("SELECT key, value, expiry FROM %s.%s WHERE key LIKE $1 ORDER BY key DESC LIMIT $2 OFFSET $3;", s.database, s.table))
+	if err != nil {
+		return errors.Wrap(err, "ReadOffset statement couldn't be prepared")
+	}
+	if s.readOffset != nil {
+		s.readOffset.Close()
+	}
+	s.readOffset = readOffset
 	write, err := s.db.Prepare(fmt.Sprintf(`INSERT INTO %s.%s(key, value, expiry)
 		VALUES ($1, $2::bytea, $3)
 		ON CONFLICT (key)
@@ -192,10 +279,16 @@ func (s *sqlStore) initDB() error {
 	if err != nil {
 		return errors.Wrap(err, "Write statement couldn't be prepared")
 	}
+	if s.write != nil {
+		s.write.Close()
+	}
 	s.write = write
 	delete, err := s.db.Prepare(fmt.Sprintf("DELETE FROM %s.%s WHERE key = $1;", s.database, s.table))
 	if err != nil {
 		return errors.Wrap(err, "Delete statement couldn't be prepared")
+	}
+	if s.delete != nil {
+		s.delete.Close()
 	}
 	s.delete = delete
 
