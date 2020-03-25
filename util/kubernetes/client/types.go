f43da9866411180990a5be36d19b8e9b7b4adf02
Asim Aslam <asim@aslam.me>
2020-03-25 20:55:49 +0000

    use requested service

--- util/kubernetes/client/types.go
+++ util/kubernetes/client/types.go
@@ -40,9 +40,9 @@ type DeploymentSpec struct {
 // DeploymentCondition describes the state of deployment
 type DeploymentCondition struct {
 	LastUpdate string `json:lastUpdateTime`
-	Type    string `json:"type"`
-	reason  string `json:"reason,omitempty"`
-	message string `json:"message,omitempty"`
+	Type       string `json:"type"`
+	reason     string `json:"reason,omitempty"`
+	message    string `json:"message,omitempty"`
 }
 
 // DeploymentStatus is returned when querying deployment
