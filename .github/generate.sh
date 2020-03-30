b1b83a3e5ea6638c230ece8756524f146000c541
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-03-30 22:51:57 +0300

    regenerate from proto
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- .github/generate.sh
+++ .github/generate.sh
@@ -1,8 +1,9 @@
 #!/bin/bash -e
 
+find . -type f -name '*.pb.*.go' -o -name '*.pb.go' -a ! -name 'message.pb.go' -delete
 PROTOS=$(find . -type f -name '*.proto')
 
 for PROTO in $PROTOS; do
   echo $PROTO
-  protoc -I./ -I$(dirname $PROTO) --micro_out=paths=source_relative:. $PROTO
+  protoc -I./ -I$(dirname $PROTO) --go_out=source_relative:. --micro_out=paths=source_relative:. $PROTO
 done
