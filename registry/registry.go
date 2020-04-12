4e539361fa704bad168305e92b7d5c0a17ab70c9
Asim Aslam <asim@aslam.me>
2020-04-12 10:58:12 +0100

    strip file

--- registry/registry.go
+++ registry/registry.go
@@ -28,6 +28,33 @@ type Registry interface {
 	String() string
 }
 
+type Service struct {
+	Name      string            `json:"name"`
+	Version   string            `json:"version"`
+	Metadata  map[string]string `json:"metadata"`
+	Endpoints []*Endpoint       `json:"endpoints"`
+	Nodes     []*Node           `json:"nodes"`
+}
+
+type Node struct {
+	Id       string            `json:"id"`
+	Address  string            `json:"address"`
+	Metadata map[string]string `json:"metadata"`
+}
+
+type Endpoint struct {
+	Name     string            `json:"name"`
+	Request  *Value            `json:"request"`
+	Response *Value            `json:"response"`
+	Metadata map[string]string `json:"metadata"`
+}
+
+type Value struct {
+	Name   string   `json:"name"`
+	Type   string   `json:"type"`
+	Values []*Value `json:"values"`
+}
+
 type Option func(*Options)
 
 type RegisterOption func(*RegisterOptions)
