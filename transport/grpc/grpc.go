9e74654b3661e7a11212a7850ad053776cca5416
Vasiliy Tolstov <v.tolstov@unistack.org>
2020-04-16 03:47:49 +0300

    fix
    Signed-off-by: Vasiliy Tolstov <v.tolstov@unistack.org>

--- transport/grpc/grpc.go
+++ transport/grpc/grpc.go
@@ -75,9 +75,6 @@ func (t *grpcTransportListener) Accept(fn func(transport.Socket)) error {
 		opts = append(opts, grpc.Creds(creds))
 	}
 
-	opts = append(opts, grpc.MaxRecvMsgSize(1024*1024*16))
-	opts = append(opts, grpc.MaxSendMsgSize(1024*1024*16))
-
 	// new service
 	srv := grpc.NewServer(opts...)
 
@@ -99,8 +96,6 @@ func (t *grpcTransport) Dial(addr string, opts ...transport.DialOption) (transpo
 
 	options := []grpc.DialOption{
 		grpc.WithTimeout(dopts.Timeout),
-		grpc.WithDefaultCallOptions(grpc.MaxCallRecvMsgSize(1024 * 1024 * 16)),
-		grpc.WithDefaultCallOptions(grpc.MaxCallSendMsgSize(1024 * 1024 * 16)),
 	}
 
 	if t.opts.Secure || t.opts.TLSConfig != nil {
