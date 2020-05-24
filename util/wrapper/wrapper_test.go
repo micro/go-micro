67146ecdc2c0d9340696348e762781b3ccb6d6ce
Ben Toogood <bentoogood@gmail.com>
2020-05-24 18:05:23 +0100

    Client Cache tests

--- util/wrapper/wrapper_test.go
+++ util/wrapper/wrapper_test.go
@@ -3,7 +3,9 @@ package wrapper
 import (
 	"context"
 	"testing"
+	"time"
 
+	"github.com/micro/go-micro/v2/client"
 	"github.com/micro/go-micro/v2/metadata"
 )
 
@@ -49,5 +51,72 @@ func TestWrapper(t *testing.T) {
 			}
 		}
 	}
+}
 
+type testClient struct {
+	callCount int
+	callRsp   interface{}
+	cache     *client.Cache
+	client.Client
+}
+
+func (c *testClient) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	c.callCount++
+	rsp = c.callRsp
+	return nil
+}
+
+func (c *testClient) Options() client.Options {
+	return client.Options{Cache: c.cache}
+}
+func TestCacheWrapper(t *testing.T) {
+	req := client.NewRequest("go.micro.service.foo", "Foo.Bar", nil)
+
+	t.Run("NilCache", func(t *testing.T) {
+		cli := new(testClient)
+		w := CacheClient(cli)
+
+		// perfroming two requests should increment the call count by two indicating the cache wasn't
+		// used even though the WithCache option was passed.
+		w.Call(context.TODO(), req, nil, client.WithCache(time.Minute))
+		w.Call(context.TODO(), req, nil, client.WithCache(time.Minute))
+
+		if cli.callCount != 2 {
+			t.Errorf("Expected the client to have been called twice")
+		}
+	})
+
+	t.Run("OptionNotSet", func(t *testing.T) {
+		cli := new(testClient)
+		w := CacheClient(cli)
+
+		// perfroming two requests should increment the call count by two since we didn't pass the WithCache
+		// option to Call.
+		w.Call(context.TODO(), req, nil)
+		w.Call(context.TODO(), req, nil)
+
+		if cli.callCount != 2 {
+			t.Errorf("Expected the client to have been called twice")
+		}
+	})
+
+	t.Run("OptionSet", func(t *testing.T) {
+		cli := &testClient{callRsp: "foobar", cache: client.NewCache()}
+		w := CacheClient(cli)
+
+		// perfroming two requests should increment the call count by once since the second request should
+		// have used the cache
+		err1 := w.Call(context.TODO(), req, nil, client.WithCache(time.Minute))
+		err2 := w.Call(context.TODO(), req, nil, client.WithCache(time.Minute))
+
+		if err1 != nil {
+			t.Errorf("Expected nil error, got %v", err1)
+		}
+		if err2 != nil {
+			t.Errorf("Expected nil error, got %v", err2)
+		}
+		if cli.callCount != 1 {
+			t.Errorf("Expected the client to be called 1 time, was actually called %v time(s)", cli.callCount)
+		}
+	})
 }
