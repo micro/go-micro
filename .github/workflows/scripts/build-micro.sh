de842c29c8f7e7579412e5af362b02420c3b2d49
Dominic Wong <dom@micro.mu>
2020-06-19 14:35:32 +0100

    add integration tests

--- .github/workflows/scripts/build-micro.sh
+++ .github/workflows/scripts/build-micro.sh
@@ -11,4 +11,11 @@ if [ $failed -gt 0 ]; then
 fi
 # unit tests
 IN_TRAVIS_CI=yes go test -v ./...
-# TODO integration tests
+
+./scripts/test-docker.sh
+pushd test
+# Generate keys for JWT tests
+ssh-keygen -f /tmp/sshkey -m pkcs8 -q -N ""
+ssh-keygen -f /tmp/sshkey -e  -m pkcs8 > /tmp/sshkey.pub
+IN_TRAVIS_CI=yes go clean -testcache && go test --tags=integration -v ./...
+popd
