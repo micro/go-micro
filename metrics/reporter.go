09fdd3c121aae50ca73bbece74985f7801e1064e
Asim Aslam <asim@aslam.me>
2020-08-18 08:29:26 +0100

    Update reporter.go

--- metrics/reporter.go
+++ metrics/reporter.go
@@ -7,7 +7,7 @@ type Tags map[string]string
 
 // Reporter is the standard metrics interface:
 type Reporter interface {
-	Count(metricName string, value int64, tags Tags) error
-	Gauge(metricName string, value float64, tags Tags) error
-	Timing(metricName string, value time.Duration, tags Tags) error
+	Count(id string, value int64, tags Tags) error
+	Gauge(id string, value float64, tags Tags) error
+	Timing(id string, value time.Duration, tags Tags) error
 }
