cf5d6bea920b337964dc7b5316a5363dacc8b748
刘海洋 <haiyang@snqu.com>
2020-08-25 04:59:59 +0800

    remove redundant code

--- util/addr/addr.go
+++ util/addr/addr.go
@@ -10,11 +10,14 @@ var (
 )
 
 func init() {
-	for _, b := range []string{"10.0.0.0/8", "172.16.0.0/12", "192.168.0.0/16", "100.64.0.0/10", "fd00::/8"} {
-		if _, block, err := net.ParseCIDR(b); err == nil {
-			privateBlocks = append(privateBlocks, block)
-		}
+	blocks := []string{
+		"10.0.0.0/8",
+		"172.16.0.0/12",
+		"192.168.0.0/16",
+		"100.64.0.0/10",
+		"fd00::/8",
 	}
+	AppendPrivateBlocks(blocks...)
 }
 
 // AppendPrivateBlocks append private network blocks
