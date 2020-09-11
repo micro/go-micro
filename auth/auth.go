eaa964097351dd843b89cd47689bf6cde56fa5ff
Ben Toogood <bentoogood@gmail.com>
2020-09-11 13:33:09 +0100

    auth: remove micro specific code

--- auth/auth.go
+++ auth/auth.go
@@ -2,14 +2,11 @@
 package auth
 
 import (
-	"context"
 	"errors"
 	"time"
 )
 
 const (
-	// BearerScheme used for Authorization header
-	BearerScheme = "Bearer "
 	// ScopePublic is the scope applied to a rule to allow access to the public
 	ScopePublic = ""
 	// ScopeAccount is the scope applied to a rule to limit to users with any valid account
@@ -116,20 +113,4 @@ type Rule struct {
 	// Priority the rule should take when verifying a request, the higher the value the sooner the
 	// rule will be applied
 	Priority int32
-}
-
-type accountKey struct{}
-
-// AccountFromContext gets the account from the context, which
-// is set by the auth wrapper at the start of a call. If the account
-// is not set, a nil account will be returned. The error is only returned
-// when there was a problem retrieving an account
-func AccountFromContext(ctx context.Context) (*Account, bool) {
-	acc, ok := ctx.Value(accountKey{}).(*Account)
-	return acc, ok
-}
-
-// ContextWithAccount sets the account in the context
-func ContextWithAccount(ctx context.Context, account *Account) context.Context {
-	return context.WithValue(ctx, accountKey{}, account)
 }
