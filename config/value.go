9dbd75f2cc1343a5ea97c9afc8bf896c127b646a
Janos Dobronszki <dobronszki@gmail.com>
2020-09-21 17:45:45 +0200

    Config interface change (#2010)

--- config/value.go
+++ config/value.go
@@ -1,49 +1,215 @@
 package config
 
 import (
+	"encoding/json"
+	"fmt"
+	"strconv"
+	"strings"
 	"time"
 
-	"github.com/micro/go-micro/v3/config/reader"
+	simple "github.com/bitly/go-simplejson"
 )
 
-type value struct{}
-
-func newValue() reader.Value {
-	return new(value)
+type JSONValues struct {
+	values []byte
+	sj     *simple.Json
 }
 
-func (v *value) Bool(def bool) bool {
+type JSONValue struct {
+	*simple.Json
+}
+
+func NewJSONValues(data []byte) *JSONValues {
+	sj := simple.New()
+
+	if err := sj.UnmarshalJSON(data); err != nil {
+		sj.SetPath(nil, string(data))
+	}
+	return &JSONValues{data, sj}
+}
+
+func NewJSONValue(data []byte) *JSONValue {
+	sj := simple.New()
+
+	if err := sj.UnmarshalJSON(data); err != nil {
+		sj.SetPath(nil, string(data))
+	}
+	return &JSONValue{sj}
+}
+
+func (j *JSONValues) Get(path string, options ...Option) Value {
+	paths := strings.Split(path, ".")
+	return &JSONValue{j.sj.GetPath(paths...)}
+}
+
+func (j *JSONValues) Delete(path string, options ...Option) {
+	paths := strings.Split(path, ".")
+	// delete the tree?
+	if len(paths) == 0 {
+		j.sj = simple.New()
+		return
+	}
+
+	if len(paths) == 1 {
+		j.sj.Del(paths[0])
+		return
+	}
+
+	vals := j.sj.GetPath(paths[:len(paths)-1]...)
+	vals.Del(paths[len(paths)-1])
+	j.sj.SetPath(paths[:len(paths)-1], vals.Interface())
+	return
+}
+
+func (j *JSONValues) Set(path string, val interface{}, options ...Option) {
+	paths := strings.Split(path, ".")
+	j.sj.SetPath(paths, val)
+}
+
+func (j *JSONValues) Bytes() []byte {
+	b, _ := j.sj.MarshalJSON()
+	return b
+}
+
+func (j *JSONValues) Map() map[string]interface{} {
+	m, _ := j.sj.Map()
+	return m
+}
+
+func (j *JSONValues) Scan(v interface{}) error {
+	b, err := j.sj.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, v)
+}
+
+func (j *JSONValues) String() string {
+	return "json"
+}
+
+func (j *JSONValue) Bool(def bool) bool {
+	b, err := j.Json.Bool()
+	if err == nil {
+		return b
+	}
+
+	str, ok := j.Interface().(string)
+	if !ok {
+		return def
+	}
+
+	b, err = strconv.ParseBool(str)
+	if err != nil {
+		return def
+	}
+
+	return b
+}
+
+func (j *JSONValue) Int(def int) int {
+	i, err := j.Json.Int()
+	if err == nil {
+		return i
+	}
+
+	str, ok := j.Interface().(string)
+	if !ok {
+		return def
+	}
+
+	i, err = strconv.Atoi(str)
+	if err != nil {
+		return def
+	}
+
+	return i
+}
+
+func (j *JSONValue) String(def string) string {
+	return j.Json.MustString(def)
+}
+
+func (j *JSONValue) Float64(def float64) float64 {
+	f, err := j.Json.Float64()
+	if err == nil {
+		return f
+	}
+
+	str, ok := j.Interface().(string)
+	if !ok {
+		return def
+	}
+
+	f, err = strconv.ParseFloat(str, 64)
+	if err != nil {
+		return def
+	}
+
+	return f
+}
+
+func (j *JSONValue) Duration(def time.Duration) time.Duration {
+	v, err := j.Json.String()
+	if err != nil {
+		return def
+	}
+
+	value, err := time.ParseDuration(v)
+	if err != nil {
+		return def
+	}
+
+	return value
+}
+
+func (j *JSONValue) StringSlice(def []string) []string {
+	v, err := j.Json.String()
+	if err == nil {
+		sl := strings.Split(v, ",")
+		if len(sl) > 1 {
+			return sl
+		}
+	}
+	return j.Json.MustStringArray(def)
+}
+
+func (j *JSONValue) Exists() bool {
 	return false
 }
 
-func (v *value) Int(def int) int {
-	return 0
+func (j *JSONValue) StringMap(def map[string]string) map[string]string {
+	m, err := j.Json.Map()
+	if err != nil {
+		return def
+	}
+
+	res := map[string]string{}
+
+	for k, v := range m {
+		res[k] = fmt.Sprintf("%v", v)
+	}
+
+	return res
 }
 
-func (v *value) String(def string) string {
-	return ""
+func (j *JSONValue) Scan(v interface{}) error {
+	b, err := j.Json.MarshalJSON()
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(b, v)
 }
 
-func (v *value) Float64(def float64) float64 {
-	return 0.0
-}
-
-func (v *value) Duration(def time.Duration) time.Duration {
-	return time.Duration(0)
-}
-
-func (v *value) StringSlice(def []string) []string {
-	return nil
-}
-
-func (v *value) StringMap(def map[string]string) map[string]string {
-	return map[string]string{}
-}
-
-func (v *value) Scan(val interface{}) error {
-	return nil
-}
-
-func (v *value) Bytes() []byte {
-	return nil
+func (j *JSONValue) Bytes() []byte {
+	b, err := j.Json.Bytes()
+	if err != nil {
+		// try return marshalled
+		b, err = j.Json.MarshalJSON()
+		if err != nil {
+			return []byte{}
+		}
+		return b
+	}
+	return b
 }
--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ replace github.com/imdario/mergo => github.com/imdario/mergo v0.3.8
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/bitly/go-simplejson v0.5.0
-	github.com/bmizerany/assert v0.0.0-20160611221934-b7ed37b82869 // indirect
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/caddyserver/certmagic v0.10.6
 	github.com/davecgh/go-spew v1.1.1
@@ -19,24 +18,22 @@ require (
 	github.com/ghodss/yaml v1.0.0
 	github.com/go-acme/lego/v3 v3.4.0
 	github.com/gobwas/httphead v0.0.0-20180130184737-2c6c146eadee
-	github.com/gobwas/pool v0.2.0 // indirect
 	github.com/gobwas/ws v1.0.3
-	github.com/gogo/protobuf v1.3.1 // indirect
 	github.com/golang/protobuf v1.4.2
-	github.com/google/uuid v1.1.1
+	github.com/google/uuid v1.1.2
 	github.com/gorilla/handlers v1.4.2
 	github.com/hashicorp/hcl v1.0.0
 	github.com/hpcloud/tail v1.0.0
 	github.com/imdario/mergo v0.3.9
 	github.com/kr/pretty v0.2.0
-	github.com/kr/text v0.2.0 // indirect
+	github.com/micro/go-micro v1.18.0
+	github.com/micro/micro/v3 v3.0.0-beta.4 // indirect
 	github.com/miekg/dns v1.1.27
 	github.com/minio/minio-go/v7 v7.0.5
-	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
 	github.com/oxtoacart/bpool v0.0.0-20190530202638-03653db5a59c
 	github.com/patrickmn/go-cache v2.1.0+incompatible
 	github.com/pkg/errors v0.9.1
-	github.com/stretchr/testify v1.5.1
+	github.com/stretchr/testify v1.6.1
 	github.com/teris-io/shortid v0.0.0-20171029131806-771a37caa5cf
 	github.com/xanzy/go-gitlab v0.35.1
 	go.etcd.io/bbolt v1.3.5
@@ -45,7 +42,5 @@ require (
 	google.golang.org/genproto v0.0.0-20200526211855-cb27e3aa2013
 	google.golang.org/grpc v1.27.0
 	google.golang.org/protobuf v1.25.0
-	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
-	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	honnef.co/go/tools v0.0.1-2019.2.3
 )
