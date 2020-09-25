4028e0156b3f439866d5d3d09b05868f0804ec7b
ben-toogood <bentoogood@gmail.com>
2020-09-25 10:02:49 +0100

    runtime: remove builder package (moved to micro) (#2023)

--- runtime/builder/builder.go
+++
@@ -1,9 +0,0 @@
-package builder
-
-import "io"
-
-// Builder is an interface for building packages
-type Builder interface {
-	// Build a package
-	Build(src io.Reader, opts ...Option) (io.Reader, error)
-}
