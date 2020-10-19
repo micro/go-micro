5f3b1daf8278ada3f366c20a21bb6a0ae552fe84
Asim Aslam <asim@aslam.me>
2020-10-19 08:19:45 +0100

    Update network.go

--- network/network.go
+++ network/network.go
@@ -6,34 +6,6 @@ import (
 	"github.com/micro/go-micro/v3/server"
 )
 
-// Error is network node errors
-type Error interface {
-	// Count is current count of errors
-	Count() int
-	// Msg is last error message
-	Msg() string
-}
-
-// Status is node status
-type Status interface {
-	// Error reports error status
-	Error() Error
-}
-
-// Node is network node
-type Node interface {
-	// Id is node id
-	Id() string
-	// Address is node bind address
-	Address() string
-	// Peers returns node peers
-	Peers() []Node
-	// Network is the network node is in
-	Network() Network
-	// Status returns node status
-	Status() Status
-}
-
 // Network is micro network
 type Network interface {
 	// Node is network node
@@ -52,4 +24,32 @@ type Network interface {
 	Client() client.Client
 	// Server is micro server
 	Server() server.Server
+}
+
+// Node is network node
+type Node interface {
+	// Id is node id
+	Id() string
+	// Address is node bind address
+	Address() string
+	// Peers returns node peers
+	Peers() []Node
+	// Network is the network node is in
+	Network() Network
+	// Status returns node status
+	Status() Status
+}
+
+// Error is network node errors
+type Error interface {
+	// Count is current count of errors
+	Count() int
+	// Msg is last error message
+	Msg() string
+}
+
+// Status is node status
+type Status interface {
+	// Error reports error status
+	Error() Error
 }
