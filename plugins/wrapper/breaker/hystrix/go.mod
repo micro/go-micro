436332cb04e419586efbbc05aa8ec2e2102f630d
qm012 <urobot@qq.com>
2021-06-20 16:00:06 +0800

    support hystrix fallback

--- plugins/wrapper/breaker/hystrix/go.mod
+++ plugins/wrapper/breaker/hystrix/go.mod
@@ -5,5 +5,12 @@ go 1.13
 require (
 	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
 	github.com/asim/go-micro/plugins/registry/memory/v3 v3.0.0-20210202145831-070250155285
-	github.com/asim/go-micro/v3 v3.0.0-20210120135431-d94936f6c97c
+	github.com/asim/go-micro/v3 v3.5.1
+	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/miekg/dns v1.1.42 // indirect
+	github.com/stretchr/testify v1.7.0 // indirect
+	golang.org/x/net v0.0.0-20210614182718-04defd469f4e // indirect
+	golang.org/x/sys v0.0.0-20210616094352-59db8d763f22 // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 )
--- plugins/wrapper/breaker/hystrix/hystrix.go
+++ plugins/wrapper/breaker/hystrix/hystrix.go
@@ -1,25 +1,37 @@
 package hystrix
 
 import (
+	"context"
+
 	"github.com/afex/hystrix-go/hystrix"
 	"github.com/asim/go-micro/v3/client"
-
-	"context"
 )
 
 type clientWrapper struct {
 	client.Client
+	fallback func(error) error
 }
 
 func (c *clientWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
 	return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
 		return c.Client.Call(ctx, req, rsp, opts...)
-	}, nil)
+	}, c.fallback)
 }
 
 // NewClientWrapper returns a hystrix client Wrapper.
-func NewClientWrapper() client.Wrapper {
+func NewClientWrapper(fallbacks ...func(error) error) client.Wrapper {
 	return func(c client.Client) client.Client {
-		return &clientWrapper{c}
+		return &clientWrapper{c, resolveFallback(fallbacks)}
+	}
+}
+
+func resolveFallback(fallbacks []func(error) error) func(error) error {
+	switch len(fallbacks) {
+	case 0:
+		return nil
+	case 1:
+		return fallbacks[0]
+	default:
+		panic("too many fallback parameters")
 	}
 }
