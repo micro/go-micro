24e6c9d5c19e228f588c07368b1a154f8427903b
Arsen Arustamyan <unvoodoo@gmail.com>
2021-09-22 01:53:15 +0300

    logger: caller's skip correction

--- plugins/logger/zap/options.go
+++ plugins/logger/zap/options.go
@@ -11,12 +11,6 @@ type Options struct {
 	logger.Options
 }
 
-type callerSkipKey struct{}
-
-func WithCallerSkip(i int) logger.Option {
-	return logger.SetOption(callerSkipKey{}, i)
-}
-
 type configKey struct{}
 
 // WithConfig pass zap.Config to logger
