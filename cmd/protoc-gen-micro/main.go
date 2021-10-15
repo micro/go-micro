9f4770e7fdfc71effbdb7532ea6cc47348d66941
Asim Aslam <asim@aslam.me>
2021-10-15 14:03:40 +0100

    fix generation (#2312)

--- cmd/protoc-gen-micro/main.go
+++ cmd/protoc-gen-micro/main.go
@@ -52,8 +52,8 @@ import (
 	"io/ioutil"
 	"os"
 
-	"go-micro.dev/cmd/protoc-gen-micro/v4/generator"
-	_ "go-micro.dev/cmd/protoc-gen-micro/v4/plugin/micro"
+	"github.com/asim/go-micro/cmd/protoc-gen-micro/v4/generator"
+	_ "github.com/asim/go-micro/cmd/protoc-gen-micro/v4/plugin/micro"
 	"google.golang.org/protobuf/proto"
 )
 
