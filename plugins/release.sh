43c2d68c00342d1ee1da8523f05d3fa958649772
Johnson C <chengqiaosheng@gmail.com>
2021-10-15 20:47:09 +0800

    release plugins

--- plugins/release.sh
+++ plugins/release.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
 tag=$1
+commitsh=$2
 
 if [ "x$tag" = "x" ]; then
   echo "must specify tag to release"
   exit 1;
 fi
 
-for m in $(find * -name 'go.mod' -exec dirname {} \;); do
-  hub release create -m "$m/$tag release" $m/$tag;
+for m in $('C:\Program Files\Git\usr\bin\find.exe' * -name 'go.mod' -exec dirname {} \;); do
+  if [ ! -n "$commitsh" ]; then
+    hub release create -m "plugins/$m/$tag release" plugins/$m/$tag;
+  else
+    hub release create -m "plugins/$m/$tag release" -t $commitsh plugins/$m/$tag;
+  fi
 done
