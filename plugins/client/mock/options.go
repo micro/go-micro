627e1f87da8775d6128ab687750db7e221c82ce6
Hunyadvári Péter <peter.hunyadvari@vcc.live>
2021-12-02 15:18:29 +0100

    Extend client mock with ability to test publish, and a few useful method like SetResponse and SetSubscriber

--- plugins/client/mock/options.go
+++ plugins/client/mock/options.go
@@ -7,11 +7,23 @@ import (
 // Response sets the response methods for a service
 func Response(service string, response []MockResponse) client.Option {
 	return func(o *client.Options) {
-		r, ok := fromContext(o.Context)
+		r, ok := responseFromContext(o.Context)
 		if !ok {
 			r = make(map[string][]MockResponse)
 		}
 		r[service] = response
-		o.Context = newContext(o.Context, r)
+		o.Context = newResponseContext(o.Context, r)
+	}
+}
+
+// Subscriber sets the subscribers service
+func Subscriber(topic string, subscriber MockSubscriber) client.Option {
+	return func(o *client.Options) {
+		r, ok := subscriberFromContext(o.Context)
+		if !ok {
+			r = make(map[string]MockSubscriber)
+		}
+		r[topic] = subscriber
+		o.Context = newSubscriberContext(o.Context, r)
 	}
 }
