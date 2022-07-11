6cf2b02f0f0f41057867ad092f222abee26d486d
Asim Aslam <asim@aslam.me>
2022-07-11 14:37:34 +0100

    move cmd to util (#2527)
    * move cmd to util
    
    * go fmt

--- config/source/cli/cli.go
+++ config/source/cli/cli.go
@@ -9,8 +9,8 @@ import (
 
 	"github.com/imdario/mergo"
 	"github.com/urfave/cli/v2"
-	"go-micro.dev/v4/cmd"
 	"go-micro.dev/v4/config/source"
+	"go-micro.dev/v4/util/cmd"
 )
 
 type cliSource struct {
