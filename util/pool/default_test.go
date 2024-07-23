46ffd07aac458998c0823150c611f85c1cd06902
Hunyadvári Péter <peter.hunyadvari@vcc.live>
2024-07-23 08:06:19 +0200

    [fix] tests

--- util/pool/default_test.go
+++ util/pool/default_test.go
@@ -73,12 +73,12 @@ func testPool(t *testing.T, size int, ttl time.Duration) {
 		// release the conn
 		p.Release(c, nil)
 
-		p.Lock()
+		p.mu.Lock()
 		if i := len(p.conns[l.Addr()]); i > size {
-			p.Unlock()
+			p.mu.Unlock()
 			t.Fatalf("pool size %d is greater than expected %d", i, size)
 		}
-		p.Unlock()
+		p.mu.Unlock()
 	}
 }
 
