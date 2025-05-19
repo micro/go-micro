3d6aa46fce373dd400b945a5f3d46a63cd39e800
Brian Ketelsen <bketelsen@gmail.com>
2025-05-19 16:37:56 -0400

    feat: full nats profile

--- profile/profile.go
+++ profile/profile.go
@@ -4,13 +4,16 @@ package profile
 import (
 	"os"
 
+	natslib "github.com/nats-io/nats.go"
 	"go-micro.dev/v5/broker"
 	"go-micro.dev/v5/broker/nats"
 	"go-micro.dev/v5/registry"
 	nreg "go-micro.dev/v5/registry/nats"
 	"go-micro.dev/v5/store"
+	nstore "go-micro.dev/v5/store/nats-js-kv"
 
 	"go-micro.dev/v5/transport"
+	ntx "go-micro.dev/v5/transport/nats"
 )
 
 type Profile struct {
@@ -34,8 +37,10 @@ func NatsProfile() Profile {
 	return Profile{
 		Registry:  nreg.NewNatsRegistry(registry.Addrs(addr)),
 		Broker:    nats.NewNatsBroker(broker.Addrs(addr)),
-		Store:     store.NewFileStore(), //  nats-backed store when available
-		Transport: transport.NewHTTPTransport(), //  nats transport when available
+		// this might be wrong, look for a better way to set this up
+		Store:     nstore.NewStore(nstore.NatsOptions(natslib.Options{Url: addr})),
+		// same, double check for single url vs slice of Server
+		Transport: ntx.NewTransport(ntx.Options(natslib.Options{Url: addr})),
 	}
 }
 
